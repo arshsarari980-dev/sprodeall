1657b1b88998ec49e8963f4bfa7c3c665a7a10b2
arshsarari980-dev <arshsarari980@gmail.com>
2026-07-14 16:53:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const cors = require("cors");
+const admin = require("firebase-admin");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  }),
+  databaseURL: "https://spro-deal-a87cd-default-rtdb.firebaseio.com"
+});
+
+const db = admin.database();
+
 app.get("/", (req, res) => {
-    res.send("Spro Backend Running");
+    res.send("Backend Running Successfully");
 });
 
 const PORT = process.env.PORT || 3000;
