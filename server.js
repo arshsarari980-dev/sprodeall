cbfbd657fdd1f90c83a7532e1403e606887a2960
arshsarari980-dev <arshsarari980@gmail.com>
2026-07-14 17:12:33 +0530

    Add express-rate-limit middleware to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const admin = require("firebase-admin");
+const rateLimit = require("express-rate-limit");
 
 const app = express();
 
